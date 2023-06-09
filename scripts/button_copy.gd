149d97020c86a578b68bf3acbd5456ccfb719c63
Thane5 <13683581+Thane5@users.noreply.github.com>
2023-06-09 17:32:57 +0200

    move copy process into seperate function

--- scripts/button_copy.gd
+++ scripts/button_copy.gd
@@ -1,9 +1,20 @@
 extends Button
 
+var sourceFileArray
+var copiedFilesArray = []
+
+
+var continueCopying = false
+var fileNumber
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _process(delta):
+	if continueCopying:
+		sourceFileArray[0].name
+		continueCopying = false
+		pass # Replace with function body.
+
+
 
 
 func _on_pressed():
@@ -17,38 +28,38 @@ func _on_pressed():
 		print("Error with target folder:" + %"Path Manager".targetFolderPath)
 	
 	if sourceExists and targetExists:
-		var fileArray = DirAccess.get_files_at(%"Path Manager".sourceFolderPath)
-		var copiedFilesArray = []
+		sourceFileArray = DirAccess.get_files_at(%"Path Manager".sourceFolderPath)
+		
 
-		for file in fileArray:
-			var sourceFilePath = %"Path Manager".sourceFolderPath + "/" + file
-			
-			# Construct the string for the Year/month subfolders
-			var dateModified = FileAccess.get_modified_time(sourceFilePath)
-			var datestring = Time.get_date_string_from_unix_time(dateModified)
-			var folderStructure ="/" + datestring.replace("-", "/").left(8)
-		
-		
-			var targetFolderPath = %"Path Manager".targetFolderPath
-			var targetFilePath = targetFolderPath + folderStructure + file
-		
-			
-			# Only copy proceed if the file doesnt already exist
-			if FileAccess.file_exists(targetFilePath):
-				print("This file already exists and will be skipped: " + targetFilePath)
-			else:
-				# Check if the folder structure exists
-				if !DirAccess.dir_exists_absolute(targetFolderPath + folderStructure):
-					DirAccess.make_dir_recursive_absolute(targetFolderPath + folderStructure)
-					print("Created Folder: " + targetFolderPath + folderStructure)
-				
-				# Copy the file
-				DirAccess.copy_absolute(sourceFilePath, targetFilePath)
-				copiedFilesArray.append(file)
-				
-			# Set log text
-			%LogArea.text = str(copiedFilesArray.size()) + " Files copied: " + str(copiedFilesArray)
-			
-			#text = "Copy 0 Files"
+		for file in sourceFileArray:
+			_copy_file(file)
 			
 
+func _copy_file(file):
+	var sourceFilePath = %"Path Manager".sourceFolderPath + "/" + file
+			
+	# Construct the string for the Year/month subfolders
+	var dateModified = FileAccess.get_modified_time(sourceFilePath)
+	var datestring = Time.get_date_string_from_unix_time(dateModified)
+	var folderStructure ="/" + datestring.replace("-", "/").left(8)
+
+
+	var targetFolderPath = %"Path Manager".targetFolderPath
+	var targetFilePath = targetFolderPath + folderStructure + file
+
+	
+	# Only copy proceed if the file doesnt already exist
+	if FileAccess.file_exists(targetFilePath):
+		print("This file already exists and will be skipped: " + targetFilePath)
+	else:
+		# Check if the folder structure exists
+		if !DirAccess.dir_exists_absolute(targetFolderPath + folderStructure):
+			DirAccess.make_dir_recursive_absolute(targetFolderPath + folderStructure)
+			print("Created Folder: " + targetFolderPath + folderStructure)
+		
+		# Copy the file
+		DirAccess.copy_absolute(sourceFilePath, targetFilePath)
+		copiedFilesArray.append(file)
+		
+	# Set log text
+	%LogArea.text = str(copiedFilesArray.size()) + " Files copied: " + str(copiedFilesArray)
