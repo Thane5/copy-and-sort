72a8ea31d195933b2059803553b4ad375f2ef9d0
Thane5 <13683581+Thane5@users.noreply.github.com>
2024-01-24 19:23:30 +0100

    Search for files in first level of subfolders

--- scripts/button_copy.gd
+++ scripts/button_copy.gd
@@ -16,7 +16,7 @@ func _process(_delta):
 	%"Set Target".disabled = continueCopying
 	
 	if continueCopying:
-		var currentFile = sourceFileArray[fileNumber]
+		var currentFilePath = sourceFileArray[fileNumber]
 		
 		# here is say "fileNumber+1" because the displayed counter should start at 1
 		var statusText = "Processing file " + str(fileNumber+1) + " of " + str(sourceFileArray.size())
@@ -25,7 +25,7 @@ func _process(_delta):
 		%"Copy Button".text = statusText
 		
 		# Copy the selected file
-		_copy_file_to_target(currentFile)
+		_copy_file_to_target(currentFilePath)
 		
 		
 		
@@ -62,12 +62,38 @@ func _on_pressed():
 		%LogArea.text = logMessage
 	
 	if sourceExists and targetExists:
-		sourceFileArray = DirAccess.get_files_at(%"Path Manager".sourceFolderPath)
-		
-		continueCopying = true
+		# First, get the files at the root directory
+		var rootFileArray = DirAccess.get_files_at(%"Path Manager".sourceFolderPath)
+		# ... and add them to the sourceFileArray
+		for rootFile in rootFileArray:
+			var rootFilePath = %"Path Manager".sourceFolderPath + "/" + rootFile
+			sourceFileArray.append(rootFilePath)
+	
+		# Now, search for any subfolders
+		var subfolders = DirAccess.get_directories_at(%"Path Manager".sourceFolderPath)
 
-func _copy_file_to_target(file):
-	var sourceFilePath = %"Path Manager".sourceFolderPath + "/" + file
+		# Go through each subfolder, and add its content to the sourceFileArray
+		for dirName in subfolders:
+			var subfolderPath = %"Path Manager".sourceFolderPath + "/" + dirName
+			
+			var subfolderFiles = DirAccess.get_files_at(subfolderPath)
+
+			# Add full path to file names
+			for fileName in subfolderFiles:
+				fileName = subfolderPath + "/" + fileName
+				sourceFileArray.append(fileName)
+		
+		# Only copy, if the there are files in the source
+		if sourceFileArray.size() > 0:
+			continueCopying = true
+		else:
+			logMessage = "0 Files found on source. Nothing to copy."
+			print(logMessage)
+			%LogArea.text = logMessage
+
+func _copy_file_to_target(filePath):
+	var sourceFilePath = filePath
+	print("attempting to copy " + str(sourceFilePath))
 			
 	# Construct the string for the Year/month subfolders
 	var dateModified = FileAccess.get_modified_time(sourceFilePath)
@@ -76,7 +102,9 @@ func _copy_file_to_target(file):
 
 
 	var targetFolderPath = %"Path Manager".targetFolderPath
-	var targetFilePath = targetFolderPath + folderStructure + file
+	
+	# get_file converts the full path to just the filename with extension
+	var targetFilePath = targetFolderPath + folderStructure + filePath.get_file()
 
 	
 	# Only copy proceed if the file doesnt already exist
@@ -90,7 +118,7 @@ func _copy_file_to_target(file):
 		
 		# Copy the file
 		DirAccess.copy_absolute(sourceFilePath, targetFilePath)
-		copiedFilesArray.append(file)
+		copiedFilesArray.append(filePath)
 		
 		
 	var filesAnalysedCount = str(sourceFileArray.size())
@@ -98,6 +126,6 @@ func _copy_file_to_target(file):
 	var copiedFilesCount = str(copiedFilesArray.size())
 	
 	logMessage = filesAnalysedCount + " files found on source. " + skippedFilesCount + " of them already exist on target. " + copiedFilesCount + " files copied: " + str(copiedFilesArray)
-	print(logMessage)
+	#print(logMessage)
 	# Set log text
 	#%LogArea.text = logMessage
